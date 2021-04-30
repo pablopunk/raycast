b95b33a51111049129e7ac7e2fd63e1c6d6d5a41
Pablo P Varela <pablo@pablopunk.com>
2021-04-30 11:38:01 +0200

    first commit 🔥

---
+++ pull-requests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title My Pull Requests
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Github
+# @raycast.icon ./images/github-logo.png
+# @raycaast.iconDark ./images/github-logo-dark.png
+
+open https://github.com/pulls/
